8483fbb4ac881d4c981c4366aa882a82b52fa385
EmersonRFilho <emerso.roseiro@gmail.com>
2022-06-07 16:46:15 -0300

    initial commit

---
+++ src/main/java/com/eds/livros/LivrosApplication.java
@@ -0,0 +1,23 @@
+package com.eds.livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+	@GetMapping("/")
+	String hello() {
+		return "Hello World";
+	}
+}
